e05042e2421c0c3c840d48bf1bfe860d641e97ee
QuirinEcker <quirin.ecker@bajupa.com>
2018-12-11 21:55:19 +0100

    optimized Project

--- script/createEntries.js
+++ script/createEntries.js
@@ -4,21 +4,44 @@ let elementsRight = new Array();
 window.addEventListener("load", () => {
 	let rightWindow = document.getElementById("right-wrapper");
 	let leftWindow = document.getElementById("left-wrapper");
-	let randomLeft = Math.floor(Math.random() * 10);
-	let randomRight = Math.floor(Math.random() * 10);
+	let namesLeft = [
+		"Brad Adella",
+		"Alice Norris",
+		"Glanville Alita",
+		"Bertina Renita",
+		"Frankie Bert",
+		"Kieron Sonnie",
+		"Jenni Wilt",
+		"Vincent Jeannine",
+		"Marty Allyn",
+		"Calista Lindsey"
+	];
 	
-	for(let i = 0; i < randomLeft; i++) {
+	let namesRight = [
+		"Rosanna Laurence",
+		"Emely Cleve",
+		"Anissa Cailin",
+		"Ryana Kam",
+		"Robina Susan",
+		"Clark Winston",
+		"Irvine Angelle",
+		"Gisselle Harvey",
+		"Lambert Philomena",
+		"Liliana Siena"
+	];
+	
+	for(let i = 0; i < namesLeft.length; i++) {
 		elementsLeft[i] = document.createElement("div");
 		elementsLeft[i].classList.add("entry");
-		elementsLeft[i].textContent = i;
+		elementsLeft[i].textContent = namesLeft[i];
 		elementsLeft[i].onclick = clickManagerLeft;
 		leftWindow.appendChild(elementsLeft[i]);
 	}
 	
-	for(let i = 0; i < randomRight; i++) {
+	for(let i = 0; i < namesRight.length; i++) {
 		elementsRight[i] = document.createElement("div");
 		elementsRight[i].classList.add("entry");
-		elementsRight[i].textContent = i;
+		elementsRight[i].textContent = namesRight[i];
 		elementsRight[i].onclick = clickManagerRight;
 		rightWindow.appendChild(elementsRight[i]);
 	}
