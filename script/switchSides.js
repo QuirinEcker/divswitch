4749c4e4e4df9aa6ed8da2a740d23cb0cab671b0
QuirinEcker <quirin.ecker@bajupa.com>
2018-12-11 21:38:30 +0100

    right works

--- script/switchSides.js
+++ script/switchSides.js
@@ -1,32 +1,31 @@
 window.onload = () => {
 	document.getElementById("left").addEventListener("click", () => {
-		let sourceWindow = document.getElementById("right-wrapper");
-		let destinationWindow = document.getElementById("left-wrapper");
-
-//		let selectedElements = document.getElementsByClassName("selected");
-//		while(selectedElements.length > 0) {
-//			selectedElements[0].classList.toggle("selected");
-//			destinationWindow.appendChild(selectedElements[0]);
-//		}
-		let sourceElements = sourceWindow.children;
-		let currentElement = 0;
-		while(currentElement < sourceElements.length) {
-			let c = sourceElements[currentElement];
-			if (c.classList.contains("selected")) {
-				destinationWindow.appendChild(c);
-				c.classList.toggle("selected");
-			} else {
-				currentElement++;
-			}
-		}
+		let rightWindow = document.getElementById("right-wrapper");
+		let leftWindow = document.getElementById("left-wrapper");
+		
+		moveElements(rightWindow, leftWindow)
 	});
 	
 	document.getElementById("right").addEventListener("click", () => {
 		let rightWindow = document.getElementById("right-wrapper");
 		let leftWindow = document.getElementById("left-wrapper");
 		
-		
+		moveElements(leftWindow, rightWindow)
 	});
+}
+
+function moveElements(sourceWindow, destinationWindow) {
+	let sourceElements = sourceWindow.children;
+	let currentElement = 0;
+	while(currentElement < sourceElements.length) {
+		let c = sourceElements[currentElement];
+		if (c.classList.contains("selected")) {
+			destinationWindow.appendChild(c);
+			c.classList.toggle("selected");
+		} else {
+			currentElement++;
+		}
+	}
 }
 
 function clickManagerLeft() {
