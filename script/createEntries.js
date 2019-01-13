d7c9ae1b1275415703a2f7b5cc8d302ea818f5da
QuirinEcker <quirin.ecker@bajupa.com>
2019-01-13 02:49:38 +0100

    adjust to scope

--- script/createEntries.js
+++ script/createEntries.js
@@ -14,36 +14,7 @@ window.addEventListener("load", () => {
 		"Calista Lindsey"
 	];
 
-	let namesRight = [
-		"Rosanna Laurence",
-		"Emely Cleve",
-		"Anissa Cailin",
-		"Ryana Kam",
-		"Robina Susan",
-		"Clark Winston",
-		"Irvine Angelle",
-		"Gisselle Harvey",
-		"Lambert Philomena",
-		"Liliana Siena"
-	];
-
 	for(let i = 0; i < namesLeft.length; i++) {
-		let element = document.createElement("div");
-		let hoverWrapper = document.createElement("div")
-		let entryWrapper = document.createElement("div")
-
-		hoverWrapper.classList.add("entry-hover");
-		entryWrapper.classList.add("entry-wrapper");
-
-		entryWrapper.appendChild(hoverWrapper);
-		hoverWrapper.appendChild(element);
-		element.classList.add("entry");
-		element.textContent = namesRight[i];
-		element.addEventListener("click", clickManager);
-		leftWindow.appendChild(entryWrapper);
-	}
-
-	for(let i = 0; i < namesRight.length; i++) {
 		let element = document.createElement("div");
 		let hoverWrapper = document.createElement("div")
 		let entryWrapper = document.createElement("div")
@@ -56,6 +27,6 @@ window.addEventListener("load", () => {
 		element.classList.add("entry");
 		element.textContent = namesLeft[i];
 		element.addEventListener("click", clickManager);
-		rightWindow.appendChild(entryWrapper);
+		leftWindow.appendChild(entryWrapper);
 	}
 });
